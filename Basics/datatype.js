c7917d779c6c47bffe415ce4dbab597953c61105
Chhavi Dubey <chhavidubey2224@gmail.com>
2026-08-19 17:50:24 +0000

    datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,21 @@
+//Primitive data type   
+//number
+//bigint -> used in big webisite, trading
+//string -> "" '' we can use both single and double quotes
+//boolean -> true/false
+//null -> its a data type as well as stand alone value ; its an object
+//undefined -> 
+//symbol ->
+
+//objects
+
+let age=12;
+
+console.log(typeof age);
+
+console.log(typeof "Hello");
+
+console.log(typeof null); //object
+
+
+console.log(typeof undefined); //undefined
